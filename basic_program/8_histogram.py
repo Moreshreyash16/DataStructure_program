962d30e771fa2245ca2c873ea88f5b059afb7887
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-11 23:34:17 +0530

    [Shreyash] Add: Write a Python program to concatenate all elements in a list into a string and return it

---
+++ basic_program/8_histogram.py
@@ -0,0 +1,19 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-07 15:35:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-07 15:35:30
+
+@Title :Write a Python program to create a histogram from a given list of integers.
+'''
+def main():
+    for i in range(5):
+        for j in range(i):
+            print("*",end=" ")
+        print()
+
+if __name__=="__main__":
+    main()
