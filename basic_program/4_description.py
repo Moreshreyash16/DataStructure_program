b9aa19e2af7c16d64cf2659406121a44239c7223
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-11 23:31:08 +0530

    [Shreyash] Add:Write a Python program to print the documents (syntax, description etc.) of Python built-in function(s).

---
+++ basic_program/4_description.py
@@ -0,0 +1,17 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-07 15:35:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-07 15:35:30
+
+@Title : Write a Python program to display the description of function.
+'''
+def main():
+    print(sorted.__doc__)
+
+
+if __name__=="__main__":
+    main()
