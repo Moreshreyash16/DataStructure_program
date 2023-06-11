4b2fc88ac72152abd8ddc59b3684ff1e68097354
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-11 23:30:27 +0530

    [Shreyash] Add:Write a Python program to display the first and last colors from the following list.

---
+++ basic_program/3_index.py
@@ -0,0 +1,18 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-07 12:35:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-07 12:35:30
+
+@Title : Write a Python program to display the first and last colors from the following list.
+'''
+def main():
+    color_list = ["Red","Green","White" ,"Black"]
+    print(color_list[0],color_list[3])
+
+
+if __name__=="__main__":
+    main()
