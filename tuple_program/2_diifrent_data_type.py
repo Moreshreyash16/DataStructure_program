3a66a1350b4fd5af42e650f77b4094ad6179541e
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-13 21:03:02 +0530

    [Shreyash] Add : Write a Python program to create a tuple with different data types.

---
+++ tuple_program/2_diifrent_data_type.py
@@ -0,0 +1,18 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-9 23:10:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-09 23:10:30
+
+@Title : Write a Python program to create a tuple with different data types
+'''
+def main():
+    tup=("hello",5,False)
+    print(f" the tuple is: {tup} ")
+
+
+if __name__=="__main__":
+    main()
