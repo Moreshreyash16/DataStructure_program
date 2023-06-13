782e107715f556f00dd43299342b18e740b92c11
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-13 21:06:07 +0530

    [Shreyash] Add : Write a Python program to reverse a tuple.

---
+++ tuple_program/10_reverse.py
@@ -0,0 +1,18 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-9 23:10:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-09 23:10:30
+
+@Title : Write a Python program to create a tuple.
+'''
+def main():
+    tup=tuple(input("Enter values : ").split())
+    print(f" the reverse of tuple is: {tup[::-1]} ")
+
+
+if __name__=="__main__":
+    main()
