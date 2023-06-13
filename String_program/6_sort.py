f0f45f4f939ddec0442a5830835dffd90f5ba0bb
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-13 21:25:36 +0530

    [Shreyash] Add : Write a Python script that takes input from the user and displays that input back in upper and lower cases.

---
+++ String_program/6_sort.py
@@ -0,0 +1,18 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-07 22:59:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-07 22:59:30
+
+@Title : Write a Python function that takes a list of words and returns the length of the longest'''
+
+def main():
+    stri=list(input("Enter a strings : ").split())
+    print(f" the sorted string is : {sorted(stri)} ")
+
+
+if __name__=="__main__":
+    main()
