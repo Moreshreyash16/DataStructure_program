4d0c2737df30abffd7f4fed2b97c099fcc95f3da
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-13 21:21:43 +0530

    [Shreyash] Add :Write a Python program to calculate the length of a string.

---
+++ String_program/1_length.py
@@ -0,0 +1,18 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-07 17:39:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-07 17:39:30
+
+@Title : Write a Python to get lenght of string
+'''
+def main():
+    str=input("Enter a string : ")
+    print(f" the length of string is : {len(str)}")
+
+
+if __name__=="__main__":
+    main()
