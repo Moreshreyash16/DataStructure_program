e94b3606c70b3fabb138a3f5a8348fcb70e206e8
Moreshreyash16 <mshreyash108@gmail.com>
2023-06-13 23:22:42 +0530

    [Shreyash] Add :  Write a Python program to iterate over dictionaries using for loops

---
+++ dictioary_program/4_iterate.py
@@ -0,0 +1,22 @@
+'''
+@Author: Shreyash More
+
+@Date: 2023-06-10 18:30:30
+
+@Last Modified by: Shreyash More
+
+@Last Modified time: 2023-06-10 18:32:30
+
+@Title : Write a Python program to iterate over dictionaries using for loops.
+
+'''
+def main():
+    my_dict = {'c': 3,'a': 1,'d': 4,'b': 2}
+    for key,value in my_dict.items():
+        print(f"The dictionary value is : {key,value}")
+if __name__=="__main__":
+    main()
+
+ 
+
+ 
